efb1531bc437604badb0f917d97438a0530d5d0d
Kartik Suba <k.suba@dsslp.com>
2018-01-24 13:48:32 +0530

    Initial Commit

--- vaadin-picture-viewer-demo/target/.generated/com/google/gwt/lang/pictureviewer_00046demo_00046DemoWidgetSet__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class pictureviewer_00046demo_00046DemoWidgetSet__EntryMethodHolder {
-  public static final void init() {
-    // Filled in by the compiler to call entry methods.
-  }
-}
